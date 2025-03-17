0110d5f75f47b9f1808668ca8da38263680a7d82
Darren Pearce <41091584+King-Swood@users.noreply.github.com>
2025-03-17 17:48:41 +1100

    Made the main app a class

--- main.py
+++ main.py
@@ -6,48 +6,47 @@ from StartupPattern import StartupPattern
 
 # if the file is called main.py, it will run automatically when the target device boots.
 
-button = None
-heartbeat = None
-startupPattern = None
+class App():
+    def __init__(self):
+        print("OctoAlarm MicroPython Version " + Globals.Version + "\n")
 
-def startup_pattern_set(value):
-    if (value > 0):
-        HAL.set_led(255)
-    else:
-        HAL.set_led(0)
+        self.button = Button(HAL.read_button)
+        self.heartbeat = Heartbeat(lambda x: HAL.set_heartbeat(x))
+        self.startupPattern = StartupPattern(self.startup_pattern_set)
+
+    @staticmethod
+    def startup_pattern_set(value):
+        if (value > 0):
+            HAL.set_led(255)
+        else:
+            HAL.set_led(0)
+
+    def loop(self):
+        self.heartbeat.update()
+        self.button.update()
+
+        if not self.startupPattern.is_finished():
+            self.startupPattern.update()
+        else:
+            HAL.set_led(128)
+        
+        # if button.just_released():
+        #     led_state = not led_state
+        #     HAL.set_led(led_state)
+        
+        # if button.is_held(500):
+        #     button.restart_held()
+        #     led_state = not led_state
+        #     HAL.set_led(led_state)
+
+app = None
 
 def setup():
-    global led_state
-    global button
-    global heartbeat
-    global startupPattern
-
-    print("OctoAlarm MicroPython Version " + Globals.Version + "\n")
-
-    button = Button(HAL.read_button)
-    heartbeat = Heartbeat(lambda x: HAL.set_heartbeat(x))
-    startupPattern = StartupPattern(startup_pattern_set)
+    global app
+    app = App()
 
 def loop():
-    global led_state
-    global button
-    global heartbeat
-    
-    heartbeat.update()
-    button.update()
-
-    if not startupPattern.is_finished():
-        startupPattern.update()
-    else:
-        HAL.set_led(128)
-    
-    # if button.just_released():
-    #     led_state = not led_state
-    #     HAL.set_led(led_state)
-    
-    # if button.is_held(500):
-    #     button.restart_held()
-    #     led_state = not led_state
-    #     HAL.set_led(led_state)
+    global app
+    app.loop()
 
 HAL.run(setup, loop)
