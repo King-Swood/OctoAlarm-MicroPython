70d1a92069d73acd35953ad2c1c2108061ea2fc1
Darren Pearce <41091584+King-Swood@users.noreply.github.com>
2025-03-19 17:59:32 +1100

    LED Pulse now works on Pico board

--- hal_rpi_pico.py
+++ hal_rpi_pico.py
@@ -6,13 +6,19 @@ import machine
 
 BUTTON_PIN = const(16)
 LED_PIN = const(25)
+LED_EXTERNAL_PIN = const(20)
 led_onboard = None
+led_external = None
 button_pin = None
 
 def init():
     global led_onboard
     led_onboard = machine.Pin(LED_PIN, machine.Pin.OUT)
     led_onboard.value(True)
+    global led_external
+    led_external = machine.PWM(machine.Pin(LED_EXTERNAL_PIN))
+    led_external.freq(5000)
+    led_external.duty_u16(0)
     global button_pin
     button_pin = machine.Pin(BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
     return
@@ -33,6 +39,8 @@ def read_button():
     return not button_pin.value()
 
 def set_led(value):
+    global led_external
+    led_external.duty_u16(int(value * 256))
     return
 
 def set_heartbeat(value):
